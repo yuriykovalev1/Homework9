6c2bbd61e8a615f8dab5009bac030c3252b86b72
Yuriy Kovalev <yuriykovalev1984@gmail.com>
2023-11-19 19:43:26 +0300

    Привет

---
+++ task64/Program.cs
@@ -0,0 +1,37 @@
+﻿//  Задача 64: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//  M = 1; N = 5 -> "2, 4"
+//  M = 4; N = 8 -> "4, 6, 8"
+
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    int result=Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintEvenNumber(int m, int n)
+{
+   if(m > n)
+   {
+      return;
+   }
+   if(m % 2 == 0)
+   {
+      Console.Write($"{m}\t ");
+   PrintEvenNumber(m + 2, n);
+   }
+   else 
+   {
+      PrintEvenNumber(m + 1, n);
+   }
+}
+
+int m=ReadInt("Введите первое число: ");
+int n=ReadInt("Введите второе число: ");
+
+PrintEvenNumber(m, n);
+
+
+
+
---
+++ task68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+//  ack(n, m)
+//    если n = 0
+//      вернуть m + 1
+//    иначе, если m = 0
+//      вернуть ack (n - 1, 1)
+//    еще
+//      вернуть ack(n - 1, ack (n, m - 1))
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    int result=Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int ack(int n, int m){
+    if(n==0){
+        return m+1;       
+    } else if(m==0){
+      return ack(n-1,1);
+    }
+    return ack(n-1, ack(n, m-1));
+}
+System.Console.WriteLine(ack(ReadInt("Введите N: "), ReadInt("Введите M: ")));
+
